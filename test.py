4c5ce8fb56dcc8530363bfa62bf0731985e1fdf1
l1b3rtaS <86371143+l1b3rtaS@users.noreply.github.com>
2023-09-18 21:57:34 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-# Функция для удаления папок с именем 'mm'
+
 def delete_mm_folders(folder_path):
     a = 0
     b = 0
@@ -10,12 +10,13 @@ def delete_mm_folders(folder_path):
             if directory == 'mm':
                 folder_to_delete = os.path.join(root, directory)
                 try:
-                    shutil.rmtree(folder_to_delete)  # Удаление пустой папки
+                    shutil.rmtree(folder_to_delete)  
                     a += 1
                 except OSError as e:
                     b += 1
     return f"DEl: {a} , NOT DEL {b}"
-# Замените 'путь_к_папке_www' на фактический путь к вашей папке 'www'
+
 wwwf = '../www'
 
-delete_mm_folders(wwwf)
+cvb = delete_mm_folders(wwwf)
+print(cvb)
